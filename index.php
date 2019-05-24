580f5cffd5de0f722842b1384857611d13b33141
Le Manh Hai <hai.lemanh.1310@gmail.com>
2019-05-24 15:08:07 +0700

    khởi tạo project

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php include "header.php"; ?>
+    <div class="uk-grid-collapse uk-grid" uk-grid="">
+        <div class="uk-width-expand uk-first-column">
+            <div class="uk-grid-collapse box5 uk-height-1-1 uk-grid" uk-grid="">
+                <div class="uk-width-auto@s uk-first-column">
+                    <div class="box6 uk-height-1-1"><img src="imgs/mu1.png" alt=""></div>
+                </div>
+                <div class="uk-width-expand@s">
+                    <div class="uk-padding-small">
+                        <h5 class="uk-margin-remove title_db">Manchester United</h5>
+                        <span>43‘ Lukaku, 67’ Raford</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="uk-width-auto">
+            <div class="uk-text-center uk-height-1-1 box3">
+                <span class="tyso uk-text-bold">2 - 2</span>
+                <div class="live1"><i class="fa fa-circle uk-text-middle" aria-hidden="true"></i> Phút 49</div>
+            </div>
+        </div>
+        <div class="uk-width-expand">
+            <div class="uk-grid-collapse box5 uk-height-1-1 uk-grid" uk-grid="">
+                <div class="uk-width-auto@s">
+                    <div class="box6 uk-height-1-1"><img src="imgs/fcb1.png" alt=""></div>
+                </div>
+                <div class="uk-width-expand@s uk-flex-first@s uk-first-column">
+                    <div class="uk-padding-small uk-text-right">
+                        <h5 class="uk-margin-remove title_db">Barcenona</h5>
+                        <span>43‘ Lukaku, 67’ Raford</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div style="position:relative; padding-bottom:56.25%; overflow:hidden;"><iframe src="https://cdn.jwplayer.com/players/az6jjFJU-6M37AJhp.html" width="100%" height="100%" frameborder="0" scrolling="auto" allowfullscreen style="position:absolute;"></iframe></div>
+<?php include "footer.php"; ?>
